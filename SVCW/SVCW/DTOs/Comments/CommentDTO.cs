19a487525dc70a9913e29b78e8f1ffc7afe1fc63
SE151245 <quyentvse151245@fpt.edu.vn>
2023-07-17 17:59:06 +0700

    Update CommentDTO.cs

--- SVCW/SVCW/DTOs/Comments/CommentDTO.cs
+++ SVCW/SVCW/DTOs/Comments/CommentDTO.cs
@@ -4,7 +4,7 @@ namespace SVCW.DTOs.Comments
 {
 	public class CommentDTO
 	{
-        public string UserID { get; set; }
+        public string UserId { get; set; }
         public string ActivityId { get; set; }
         [RegularExpression(@"^(?!.*(fuck|badword1|badword2|địt|đụ|lồn|cặc|chém|loz|Đm|Duma|Nứng|Ngáo)).*$")]
         public string CommentContent { get; set; }
