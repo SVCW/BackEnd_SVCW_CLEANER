5bbe59c1292f60334987e504fd3e32c775e0e664
Phat Huynh <phatst1232@gmail.com>
2023-08-02 18:52:19 +0700

    Update Hosted DB --connectionString
    Update connectionString connect to deployed Database server

--- SVCW/SVCW/Models/SVCWContext.cs
+++ SVCW/SVCW/Models/SVCWContext.cs
@@ -46,7 +46,7 @@ namespace SVCW.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source=LAPTOP-8LC85HGU\\SQLEXPRESS;Initial Catalog=SVCW;Persist Security Info=True;User ID=sa;Password=12");
+                optionsBuilder.UseSqlServer("Data Source=svcw.database.windows.net;Initial Catalog=SVCW;User ID=svcw;Password=zubfeh-roGfi3-jihgid;TrustServerCertificate=true;");
             }
         }
 
