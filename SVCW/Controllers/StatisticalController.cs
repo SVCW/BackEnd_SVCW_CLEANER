c92763fe317bbdb99db67c6c0d303b653622cf85
SE151245 <quyentvse151245@fpt.edu.vn>
2023-08-09 21:50:50 +0700

    statistical

---
+++ SVCW/Controllers/StatisticalController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SVCW.DTOs.Activities;
+using SVCW.DTOs;
+using SVCW.Interfaces;
+using SVCW.DTOs.Statistical;
+
+namespace SVCW.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatisticalController : ControllerBase
+    {
+        private IStatistical service;
+        public StatisticalController(IStatistical service)
+        {
+            this.service = service;
+        }
+
+        [Route("statistical")]
+        [HttpPost]
+        public async Task<IActionResult> get(string userId, DateTime start, DateTime end)
+        {
+            ResponseAPI<StatisticalUserDonateDTO> responseAPI = new ResponseAPI<StatisticalUserDonateDTO>();
+            try
+            {
+                responseAPI.Data = await this.service.get(userId,start,end);
+                return Ok(responseAPI);
+            }
+            catch (Exception ex)
+            {
+                responseAPI.Message = ex.Message;
+                return BadRequest(responseAPI);
+            }
+        }
+    }
+}
