32be2ac4d6c68f8ef077f19644a176c82f5f7c3b
Phat Huynh <phatst1232@gmail.com>
2023-08-12 11:51:00 +0700

    Send email with: <html> data body - subject - toEmail

---
+++ SVCW/DTOs/Email/EmailDTO.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace SVCW.DTOs.Email
+{
+    //public class 
+
+	public class SendEmailReqDTO
+	{
+        public string? sendTo { get; set; }
+        public string? subject { get; set; }
+        public string? body { get; set; }
+    }
+
+    public class SendEmailResDTO
+    {
+        public bool isSuccess { get; set; }
+        public string? errorMessage { get; set; }
+    }
+}
+
---
+++ SVCW/Interfaces/IEmail.cs
@@ -0,0 +1,10 @@
+﻿using SVCW.DTOs.Email;
+
+namespace SVCW.Interfaces
+{
+	public interface IEmail
+	{
+        Task<SendEmailResDTO> sendEmail(SendEmailReqDTO dto);
+	}
+}
+
