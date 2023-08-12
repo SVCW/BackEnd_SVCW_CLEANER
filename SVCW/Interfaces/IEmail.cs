5000b54f2197d3db7e503f6e5837e9fc38560660
Phat Huynh <phatst1232@gmail.com>
2023-08-12 21:23:09 +0700

    Add API - Send email with tamplate

--- SVCW/Interfaces/IEmail.cs
+++ SVCW/Interfaces/IEmail.cs
@@ -5,6 +5,7 @@ namespace SVCW.Interfaces
 	public interface IEmail
 	{
         Task<SendEmailResDTO> sendEmail(SendEmailReqDTO dto);
+		Task<SendEmailResDTO> sendEmailWithTamplate(SendEmailWithTamplateReqDTO dto);
 	}
 }
 
